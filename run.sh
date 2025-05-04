ee051baec62705a3213f6e104a236a3babd66ed5
RebornEnder <enderop44rblx+bruh@gmail.com>
2025-05-05 00:04:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -y
-sudo apt-get install openssh-server -y
+sudo apt-get install -y openssh-server
 sudo systemctl enable ssh --now
-sudo systemctl start ssh
 sudo useradd -m -s /bin/bash ender
 echo "ender:ender" | sudo chpasswd
 sudo usermod -aG sudo ender
+echo "User 'ender' created with sudo privileges at $(date)" >> /var/log/user_setup.log
